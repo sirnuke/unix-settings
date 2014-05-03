d02b60dd9bf2b1bfbc15f9160e07a2bb2d7ef85f
Bryan DeGrendel <bryan@degrendel.com>
2014-05-02 23:43:29 -0400

    Added a few more packages to Ubuntu setup script

--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt-get install zsh meld ack-grep ruby clang build-essential
+sudo apt-get install zsh meld ack-grep ruby clang build-essential git mercurial
 sudo ln -s /usr/bin/ack-grep /usr/bin/ack
