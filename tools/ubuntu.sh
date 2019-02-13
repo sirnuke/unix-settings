54b40237434b60daa7e25d32ebd37700c6ed7102
Bryan DeGrendel <bryan.degrendel@soartech.com>
2019-02-13 14:18:20 -0500

    Update Ubuntu script to actually setup the system

--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-sudo apt-get install zsh meld ack-grep ruby clang build-essential git mercurial
-sudo ln -s /usr/bin/ack-grep /usr/bin/ack
+set -e
+set -x
+
+sudo apt update
+sudo apt install -y zsh meld build-essential git vim silversearch-ag libelf-dev dkms curl
+
+cp -i vimrc ~/.vimrc
+cp -i -r vim ~/.vim
+cp -i zshrc ~/.zshrc
+cp -i zlogout ~/.zlogout
+cp -i zlocal ~/.zlocal
+cp -i -r fonts ~/.fonts
+cp -i nanorc ~/.nanorc
+cp -i tmux.conf ~/.tmux.conf
+
+chsh -s /bin/zsh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
